ef0031e8e51a0eeca11d284cd0a59812c3a2e351
queens kisivuli <queenskisivuli@gmail.com>
2023-08-21 14:34:48 +0300

    db migrations

---
+++ Migrations/20230821103215_updeate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Product_Shop.Migrations
+{
+    /// <inheritdoc />
+    public partial class updeate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discription",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discription",
+                table: "Products");
+        }
+    }
+}
---
+++ Migrations/20230821110417_schemers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Product_Shop.Migrations
+{
+    /// <inheritdoc />
+    public partial class schemers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
